bdf999f619cb68ac7c33843056018bf2467caa47
Zoltan Varro <varroz56@gmail.com>
2021-01-28 23:53:10 +0000

    fixed path and added str to model

--- profiles/models.py
+++ profiles/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # https://docs.djangoproject.com/en/3.1/ref/models/fields/
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    return 'images/profile_picture/user_{0}/{1}'.format(instance.user.id, filename)
+    return 'images/profile_pictures/user_{0}/{1}'.format(instance.user.id, filename)
 
 
 # User Profile stores User related not billing address information
@@ -18,3 +18,6 @@ class UserProfile(models.Model):
     profile_picture = models.ImageField(
         default='images/profile_pictures/userlight.png', upload_to=user_directory_path, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username

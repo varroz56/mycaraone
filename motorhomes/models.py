6ba8c3c0bf6fad38b1577036d9cf4a91e5261ac5
Zoltan Varro <varroz56@gmail.com>
2021-01-30 09:49:54 +0000

    vehicle model

--- motorhomes/models.py
+++ motorhomes/models.py
@@ -5,3 +5,19 @@ from django.db import models
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255)
     website = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+# Vehicle stores information about the caravan/van
+
+
+class Vehicle(models.Model):
+    make = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    model = models.CharField(max_length=255)
+    length = models.IntegerField()
+    width = models.IntegerField()
+    height = models.IntegerField()
+
+    def __str__(self):
+        return self.model

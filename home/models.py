9a7ef18ab19613642bb5e84e919192caadae5ee9
Zoltan Varro <varroz56@gmail.com>
2021-01-31 21:55:56 +0000

    contact model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Contact message model
+
+
+class Contact(models.Model):
+    """ A model to store messages from the contact form"""
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.email

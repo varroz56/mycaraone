c47e1c1809a3b988264e3cd90de135faa616baca
Zoltan Varro <varroz56@gmail.com>
2021-01-31 02:25:00 +0000

    added detailed page and basic layout

--- motorhomes/urls.py
+++ motorhomes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import MotorhomeListView
+from .views import MotorhomeListView, MotorhomeDetailedView
 
 urlpatterns = [
     path('', MotorhomeListView, name='motorhomes'),
+    path('details/<int:pk>', MotorhomeDetailedView, name='motorhome_details'),
 ]
--- motorhomes/views.py
+++ motorhomes/views.py
@@ -10,3 +10,13 @@ def MotorhomeListView(request):
     context = {
         'motorhomes': motorhomes}
     return render(request, 'motorhomes/motorhomes.html', context)
+
+
+# A View to show details about the chosen motorhome
+def MotorhomeDetailedView(request, pk):
+    motorhome=Motorhome.objects.get(id=pk)
+
+    context={
+        'motorhome': motorhome,
+    }
+    return render(request, 'motorhomes/motorhome_details.html', context)

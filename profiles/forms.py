7510327566510f4ecb27055780aec29d4420dd2d
Zoltan Varro <varroz56@gmail.com>
2021-01-31 11:32:39 +0000

    added billing address to profile page

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from .models import UserProfile
-
+from checkout.models import BillingAddress
 # A form to handle the user profile view,
 # in this way excluded all non-createable or changeable
 # fields and will be able to handle create read update
@@ -11,3 +11,32 @@ class UserProfileUpdateForm(ModelForm):
     class Meta:
         model = UserProfile
         fields = ['phone', 'profile_picture']
+
+
+# Billing address form
+
+class BillingAddressForm(ModelForm):
+    class Meta:
+        model = BillingAddress
+        exclude = ['user']
+
+    def __init__(self, *srgs, **kwargs):
+        """ Placeholders for the formfields"""
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number':'Phone Number',
+            'address_line1':'Address Line 1',
+            'address_line2':'Address Line 2',
+            'postcode':'Postcode',
+            'city':'City',
+            'country':'Country',
+        }
+        # https://docs.djangoproject.com/en/3.1/ref/forms/widgets/
+        # Set autofocus on the name
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            self.fields[field].widget.attrs['placeholder'] = placeholders[field]
+            self.fields[field].widget.attrs['class'] = 'billing-address-field'
+            self.fields[field].required = True
+            self.fields[field].label = False
--- profiles/views.py
+++ profiles/views.py
@@ -1,28 +1,31 @@
 from django.shortcuts import render
-
-from .forms import UserProfileUpdateForm
+from checkout.models import BillingAddress
+from .forms import UserProfileUpdateForm, BillingAddressForm
 # A view to show user profile page
 
 
 def UserProfileView(request):
     profile = request.user.userprofile
-
+    if BillingAddress.objects.filter(user=request.user).exists():
+        billingaddress = BillingAddress.objects.get(user=request.user)
+    else:
+        billingaddress = None
     context = {
         'profile': profile,
+        'billingaddress': billingaddress,
     }
     return render(request, 'profiles/myprofile.html', context)
-
-
 
 
 def UserProfileUpdateView(request):
     profile = request.user.userprofile
     form = UserProfileUpdateForm(instance=profile)
     if request.method == 'POST':
-        form = UserProfileUpdateForm(request.POST, request.FILES, instance=profile)
+        form = UserProfileUpdateForm(
+            request.POST, request.FILES, instance=profile)
         if form.is_valid():
             form.save()
-            
+
     context = {
         'profile': profile,
         'form': form

7db8a9b0da1516639f1e911f34446d27c9b17b13
Zoltan Varro <varroz56@gmail.com>
2021-02-01 15:21:33 +0000

    contact form with email, index page structure

--- home/views.py
+++ home/views.py
@@ -34,16 +34,77 @@ def ContactFormView(request):
             'subject': request.POST['subject'],
             'message': request.POST['message'],
         }
+        motorhomes = Motorhome.objects.all()
+        form = ContactMessageForm
+        context = {
+            'motorhomes': motorhomes,
+            'contactform': form,
+        }
+        template = 'home/index.html'
         try:
             # Create and save new ContactUsMessage instance
             message = ContactUsMessage(
-                name=form_data['email'],
+                name=form_data['name'],
                 email=form_data['email'],
                 subject=form_data['subject'],
                 message=form_data['message'])
             message.save()
-            messages.add_message(request, messages.SUCCESS, 'We saved your message')
+            messages.add_message(request, messages.SUCCESS,
+                                 'We saved your message')
+            try:
+                # setting end and prefix vars
+                end = str(message.id)
+                prefix = "CMF"
+                # in the case of over 9999999 insatnces
+                if message.id > 9999999:
+                    # backup prefix and the end will roll back to start
+                    # from 1
+                    prefix = "CMO"
+                    end = str(message.id-9999999)
+                # get the length of the id
+                # fill with 0s while get the required length
+                num = len(end)
+                mid = ""
+                while num < 7:
+                    mid += "0"
+                    num += 1
+                # set ref from the required tags
+                ref = prefix + mid + end
+            except:
+                ref = 'No Reference was created, apologies for the inconvenience'
+            try:
+                print('in email section')
+                print(message)
+                print(ref)
+                # as Django send_mail required fields
+                # name
+                name = form_data['name']
+                # subject
+                subject = ref + " Re: " + form_data['subject']
+                # our message
+                message = "Dear " + name + ",\n" + "Thank you for contacting MyCaraOne, your enquery reference number: " + \
+                    ref + ".\n" + "We will get back to you shortly. \n" + \
+                    "Kind Regards, \n" + " MyCaraOne Team"
+                # get the email host to set sender
+                sender = settings.EMAIL_HOST_USER
+                recipient = [form_data['email']]
+                send_mail(
+                    subject,
+                    message,
+                    sender,
+                    recipient,
+                    fail_silently=False
+                )
+                # message sent successfully
+                messages.add_message(request, messages.SUCCESS,
+                                     'We have saved your message, please check your email for our response')
+                return render(request, template, context)
+            except:
+                messages.add_message(request, messages.ERROR,
+                                     'Sorry, We were unable to send you a response')
+                return render(request, template, context)
         except:
-            messages.add_message(request, messages.ERROR, 'Sorry, We were unable to save your message')
-
-    return render(request, IndexView)
+            messages.add_message(request, messages.ERROR,
+                                 'Sorry, We were unable to save your message')
+            return render(request, template, context)
+    return render(request, template, context)

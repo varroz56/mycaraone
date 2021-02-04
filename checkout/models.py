a6a19ce51142224338cfee6f702f05557294e51d
Zoltan Varro <varroz56@gmail.com>
2021-02-04 02:32:24 +0000

    checkout page without card

--- checkout/models.py
+++ checkout/models.py
@@ -6,16 +6,16 @@ from django.contrib.auth.models import User
 
 class BillingAddress(models.Model):
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
 
     full_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
     phone_number = models.CharField(max_length=50)
     address_line1 = models.CharField(max_length=255)
-    address_line2 = models.CharField(max_length=255)
+    address_line2 = models.CharField(max_length=255, blank=True, null=True)
     postcode = models.CharField(max_length=15)
     city = models.CharField(max_length=200)
     country = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"{self.full_name} - {self.user.username}"
+        return f"{self.full_name} - {self.user}"
--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.conf import settings
+
+from .models import BillingAddress
+from motorhomes.models import Motorhome
 # A Checkout view
 
 
 def CheckoutView(request):
-    return render(request, 'checkout/checkout.html')
+    user=request.user
+    if BillingAddress.objects.filter(user=user):
+        billingaddress = BillingAddress.objects.get(user=user)
+
+    motorhome = Motorhome.objects.get(pk=1)
+    context = {
+        'billingaddress': billingaddress,
+        'motorhome': motorhome,
+    }
+
+    return render(request, 'checkout/checkout.html', context)

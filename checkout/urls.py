6c7372df6ebcb5a5e8875b7bd08f07acbb8572c7
Zoltan Varro <varroz56@gmail.com>
2021-02-05 23:05:49 +0000

    added webhooks(course content), tested with cli

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import CheckoutView, CheckoutSuccessView
-
+from .webhooks import webhook
 
 urlpatterns = [
     path('', CheckoutView, name='checkout'),
     path('success/', CheckoutSuccessView, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -11,3 +11,13 @@ class StripeWH_Handler:
         """ Handle generic/unknown/unexpected webhook event """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}', status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """ Handle payment_intent.payment_succeeded event """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}', status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """ Handle payment_intent.payment_failed event """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}', status=200)

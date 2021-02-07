a25d4256a14d2ac5dc8c6efe689e49856bc47de6
Zoltan Varro <varroz56@gmail.com>
2021-02-07 23:33:04 +0000

    stripe elements error

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -45,67 +45,70 @@ card.addEventListener("change", function (event) {
 });
 
 // get the form object by id
-var form = document.getElementById('payment-form');
+var form = document.getElementById("payment-form");
 // awaitng to submit the form
-form.addEventListener('submit', function(ev) {
+form.addEventListener("submit", function (ev) {
   // disable card update, form fade out, start the spinner
-    ev.preventDefault();
-    card.update({ 'disabled': true});
-    $('#submit-button').attr('disabled', true);
-    $('#payment-form').fadeToggle("slow");
-    $('#loading-overlay').fadeToggle(70);
- 
- 
-    // using the csrf token
-    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
-    // define postData form the csrf token and the client secret
-    var postData = {
-        'csrfmiddlewaretoken': csrfToken,
-        'client_secret': client_secret,
-    };
-    //define url for the post
-    var url = '/checkout/cache_checkout_data/';
-    // post data and awaitng for confirmation, that the intent was updated successfully
-    $.post(url, postData).done(function () {
+  ev.preventDefault();
+  card.update({ disabled: true });
+  $("#submit-button").attr("disabled", true);
+  $("#payment-form").fadeToggle("slow");
+  $("#loading-overlay").fadeToggle(70);
+
+  // using the csrf token
+  var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+  // define postData form the csrf token and the client secret
+  var postData = {
+    csrfmiddlewaretoken: csrfToken,
+    client_secret: client_secret,
+  };
+  //define url for the post
+  var url = "/checkout/cache_checkout_data/";
+  // post data and awaitng for confirmation, that the intent was updated successfully
+  $.post(url, postData)
+    .done(function () {
       //stripe confirm payment metyhod adding billing address
-        stripe.confirmCardPayment(client_secret, {
-            payment_method: {
-                card: card,
-                billing_details: {
-                  name: $.trim(form.full_name.value),
-                  phone: $.trim(form.phone_number.value),
-                  email: $.trim(form.email.value),
-                  address: {
-                      line1: $.trim(form.street_number.value),
-                      line2: $.trim(form.route.value),
-                      city: $.trim(form.locality.value),
-                      country: $.trim(form.country.value),
-                  }
-                }
+      stripe
+        .confirmCardPayment(client_secret, {
+          payment_method: {
+            card: card,
+            billing_details: {
+              name: $.trim(form.full_name.value),
+              phone: $.trim(form.phone_number.value),
+              email: $.trim(form.email.value),
+              address: {
+                line1: $.trim(form.street_number.value),
+                line2: $.trim(form.route.value),
+                city: $.trim(form.locality.value),
+                country: $.trim(form.country.value),
+              },
             },
-        }).then(function(result) {
-            // if there is any issue
-            if (result.error) {
-                var errorDiv = document.getElementById('card-errors');
-                var html = `
+          },
+        })
+        .then(function (result) {
+          // if there is any issue
+          if (result.error) {
+            var errorDiv = document.getElementById("card-errors");
+            var html = `
                     <span class="icon" role="alert">
                     <i class="fas fa-exclamation-triangle"></i>
                     </span>
                     <span>${result.error.message}</span>`;
-                $(errorDiv).html(html);
-                $('#payment-form').fadeToggle(100);
-                $('#loading-overlay').fadeToggle(100);
-                card.update({ 'disabled': false});
-                $('#submit-button').attr('disabled', false);
-            } else {
-              // the payment was successful, the form can be submitted
-                if (result.paymentIntent.status === 'succeeded') {
-                    form.submit();
-                }
+            $(errorDiv).html(html);
+            $("#payment-form").fadeToggle(100);
+            $("#loading-overlay").fadeToggle(100);
+            card.update({ disabled: false });
+            $("#submit-button").attr("disabled", false);
+          } else {
+            // the payment was successful, the form can be submitted
+            if (result.paymentIntent.status === "succeeded") {
+              form.submit();
             }
+          }
         });
-    }).fail(function () {
-        //if the post fails reload the page
-        location.reload();
     })
+    .fail(function () {
+      //if the post fails reload the page
+      location.reload();
+    });
 });
